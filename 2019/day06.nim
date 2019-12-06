79f7c5564a346c831d34a74d4c86faeeeec1c502
Pietro Peterlongo <pietro.peterlongo@gmail.com>
2019-12-06 23:11:05 +0100

    completed  2019/day06 solution

--- 2019/day06.nim
+++ 2019/day06.nim
@@ -1,4 +1,4 @@
-import sets, tables, sequtils, strutils, strformat, hashes
+import sets, tables, sequtils, strutils, strformat, hashes, math
 
 let
   input = "2019/day06.txt".readFile.splitLines
@@ -13,13 +13,14 @@ type
     name: string
     sun: AstroBody
     moons: seq[AstroBody]
-  Universe = HashSet[AstroBody]
+  # Universe = HashSet[AstroBody]
+  Map = Table[string, AstroBody]
 
 proc isRoot(obj: AstroBody): bool =
   if obj.sun == nil:
     return true
 
-proc isLeave(obj: AstroBody): bool =
+proc isLeaf(obj: AstroBody): bool =
   if obj.moons.len == 0:
     return true
 
@@ -32,21 +33,20 @@ proc repr(obj: AstroBody): string =
     result.add "Root "
   else:
     result.add "(" & $obj.sun & " "
-  if obj.isLeave:
-    result.add "Leave"
+  if obj.isLeaf:
+    result.add "Leaf"
   else:
     result.add $obj.moons
 
-proc hash(obj: AstroBody): Hash = hash(obj,name)
+# proc hash(obj: AstroBody): Hash = hash(obj.name)
 
 var com = AstroBody(name: "COM")
 
 echo com
 echo com.repr
 
-var uom: Universe
-
-uom.incl(com)
+# var uom: Universe
+# uom.incl(com)
 
 proc parseOrbit(l: string): tuple[sun: string, moon: string] =
   let s = l.split(")")
@@ -57,4 +57,93 @@ proc parseOrbit(l: string): tuple[sun: string, moon: string] =
 echo "COM)B".parseOrbit
 echo "21X)BMW".parseOrbit
 
+proc parseMap(input: seq[string]): Map =
+  var sun, moon: AstroBody
+  for line in input:
+    let
+      orbit = line.parseOrbit
+    if result.hasKey(orbit.sun):
+      sun = result[orbit.sun]
+    else:
+      sun = AstroBody(name: orbit.sun)
+    if result.hasKey(orbit.moon):
+      moon = result[orbit.moon]
+    else:
+      moon = AstroBody(name: orbit.moon)
+    sun.moons.add moon
+    if moon.sun != nil:
+      raise ValueError.newException fmt"double sun found: {moon.sun}){moon}({sun}"
+    moon.sun = sun
+    result[orbit.sun] = sun
+    result[orbit.moon] = moon
 
+let map = test.parseMap
+
+for body in map.values:
+  echo body.repr
+
+proc findRoot(map: Map): AstroBody =
+  for body in map.values:
+    if body.isRoot:
+      return body
+
+let root = map.findRoot
+
+echo root
+
+proc countAllOrbits(body: AstroBody): int =
+  if body.isRoot:
+    return 0
+  result = 1 + body.sun.countAllOrbits  
+
+proc countAllOrbits(map: Map): int =
+  for body in map.values:
+    result += body.countAllOrbits
+
+echo "part 1 (test): ", map.countAllOrbits
+let realMap = input.parseMap
+echo "part 1: ", realMap.countAllOrbits
+
+let
+  newTest = """COM)B
+B)C
+C)D
+D)E
+E)F
+B)G
+G)H
+D)I
+E)J
+J)K
+K)L
+K)YOU
+I)SAN"""
+  newTestMap = newTest.splitLines.parseMap
+  testMe = newTestMap["YOU"]
+  testSanta = newTestMap["SAN"]
+  me = realMap["YOU"]
+  santa = realMap["SAN"]
+
+proc suns(body: AstroBody): seq[string] =
+  if body.isRoot:
+    return @[]
+  return @[body.sun.name] & body.sun.suns
+
+echo testMe.suns, testMe.suns.len
+echo testSanta.suns, testSanta.suns.len
+
+proc distance(a, b: AstroBody): int =
+  let
+    aSuns = a.suns
+    bSuns = b.suns
+  var i = 1
+  while true:
+    if aSuns[^i] == bSuns[^i]:
+      inc i
+    else:
+      break
+  dec i
+  return aSuns.len + bSuns.len - 2*i
+
+echo "part 2 (test): ", testMe.distance(testSanta)
+echo "part 2: ", me.distance(santa)
