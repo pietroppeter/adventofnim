e63ef5799225fb4da96d000befe76f92541f3981
Pietro Peterlongo <pietro.peterlongo@gmail.com>
2019-12-15 23:23:19 +0100

    added 2019/day12 part1 solution

--- 2019/day12.nim
+++ 2019/day12.nim
@@ -1,4 +1,4 @@
-import strscans, strformat, math, strutils, sequtils
+import strscans, strformat, math, strutils, sequtils, algorithm
 
 let inp1 = """<x=-1, y=0, z=2>
 <x=2, y=-10, z=-7>
@@ -99,3 +99,164 @@ echo bodies2.energy
 
 bodies0.step 1000
 echo "part 1: ", bodies0.energy
+
+# part 2
+# problem can be reduced to one dimension (x)
+type
+  NamedMoon = tuple[x, v, g: int, name: string]
+  JupyMoons = array[4, NamedMoon]
+
+proc cmp(x, y: NamedMoon): int =
+  if x < y: -1 else: 1
+
+# overloaded sort does not seem to work with array!
+proc sort(m: var JupyMoons) =
+  m.sort(cmp)
+
+proc collision(m: JupyMoons): bool =
+  # assume sorted
+  m[0].x == m[1].x or m[1].x == m[2].x or m[2].x == m[3].x
+
+proc updateGravity(m1, m2: var NamedMoon) =
+  if m1.x < m2.x:
+    inc m1.g
+    dec m2.g
+  elif m1.x > m2.x:
+    dec m1.g
+    inc m2.g
+
+proc computeGravity(m: var JupyMoons) =
+  for i in 0 .. 3:
+    m[i].g = 0
+  for i, j in allPairs(4):
+    updateGravity(m[i], m[j])
+
+proc applyGravity(m: var JupyMoons) =
+  for i in 0 .. 3:
+    m[i].v += m[i].g
+    
+proc applyVelocity(m: var JupyMoons) =
+  for i in 0 .. 3:
+    m[i].x += m[i].v
+
+proc step(m: var JupyMoons) =
+  computeGravity m
+  applyGravity m
+  applyVelocity m
+
+proc jump(m: var NamedMoon, s: Natural) =
+  # jump ahead to time s with constant gravity, s is chosen so that gravity will not change
+  m.x += m.v*s + m.g*s*(s + 1) div 2
+  m.v += m.v + m.g*s
+
+proc jump(m: var JupyMoons, s: Natural) =
+  for i in 0 .. 3:
+    jump m[i], s
+
+proc minCrossTime(m1, m2: NamedMoon): Natural =
+  # assume m1.x < m2.x
+  # I know that at time t:
+  # m1.x(t) = m1.x + m1.v*t + m1.g*t*(t + 1) div 2
+  # and similar for m2.x(t) where:
+  # I have that m2.g = m1.g - 2
+  # find minimal s > 0 such that m1.x(s) >= m2.x(s)
+  # the eq for which m1.x(s) == m2.x(s) is:
+  # (m2.x - m1.x) + (m2.v - m1.v)*t - t*(t +1) = 0  (since m2.g - m1.g -> -2)
+  # t^2 + (m1.v - m2.v + 1)*t + (m1.x - m2.x)  and we have a = 1 and c < 0 so we have two real solution and the only positive one is:
+  # (-b + sqrt(b^2 -4c))/2
+  let
+    b = m1.v - m2.v + 1
+    c = m1.x - m2.x
+    delta = b^2 - 4*c
+    t = 0.5*(-b.float + sqrt(delta.float))
+  ceil(t).int
+
+proc minCrossTime(m: JupyMoons): Natural =
+  var s: int
+  result = Natural.high
+  for i in 0 .. 2:
+    s = minCrossTime(m[i], m[i + 1])
+    if s < result:
+      result = s
+
+proc isPeriodReachable(m1, m2: NamedMoon, s: var int): bool =
+  # m1.x(t) = m2.x (does not move, it is initial position)
+  # m1.g*t^2 + (2*m1.v + m1.g)*t + 2*(m1.x - m2.x) = 0
+  var
+    m1 = m1
+    t: int
+  let
+    a = m1.g
+    b = 2*m1.v + m1.g
+    c = 2*(m1.x - m2.x)
+    delta = b^2 - 4*a*c
+  if delta < 0:
+    return false
+  t = (0.5*(b.float + sqrt(delta.float))/a.float).int
+  jump m1, t
+  if m1 != m2:
+    return false
+  s = t
+  return true
+
+proc isPeriodReachable(m, moons: JupyMoons, s: var int): bool =
+  var m = m
+  # first they will have to be in same order:
+  for i in 0 .. 3:
+    if m[i].name != moons[i].name:
+      return false
+  if not isPeriodReachable(m[0], moons[0], s):
+    return false
+  for i in 1 .. 3:
+    jump m[i], s
+    if m[i] != moons[i]:
+      return false
+
+proc period(moons: JupyMoons): int =
+  assert moons.isSorted(cmp)
+  var
+    m = moons
+    t = 1
+    s: int
+  step m
+  sort m
+  while m != moons:
+    echo "t = " & $t
+    if m.collision:
+      echo "collision at t = " & $t
+      step m
+      sort m
+      inc t
+      continue
+    s = minCrossTime(m)
+    echo "mincrosstime: " & $s
+    if isPeriodReachable(m, moons, s):
+      # if true, s will be changed to time to period
+      return t + s
+    jump m, s
+    t += s
+    sort m
+
+bodies1 = inp1.parseBodies
+
+proc to3dJupyMoons(b: Bodies): array[3, JupyMoons] =
+  for i in 0 .. 3:
+    result[0][i].x = b[i].pos.x
+    result[1][i].x = b[i].pos.y
+    result[2][i].x = b[i].pos.z
+    result[0][i].v = b[i].vel.x
+    result[1][i].v = b[i].vel.y
+    result[2][i].v = b[i].vel.z
+  for j in 0 .. 2:
+    sort result[j]
+
+var jm1 = bodies1.to3dJupyMoons
+
+proc period(b: array[3, JupyMoons]): int =
+  var p: seq[int]
+  for i in 0 .. 2:
+    p.add period b[i]
+    echo p[i]
+  return lcm(lcm(p[0], p[1]), p[2])
+
+echo "period test 1: ", period jm1
