47451795a26c69c893f124d54f5731b31cae6395
Pietro Peterlongo <pietro.peterlongo@gmail.com>
2021-12-01 23:44:37 +0100

    improved visualization (but then I broke something)

--- 2021/day01.nim
+++ 2021/day01.nim
@@ -3,7 +3,7 @@ import nimib, animu, nimoji
 nbInit(theme=useAdventOfNim)
 nbText: """## 2021, day 1: [Sonar Sweep](https://adventofcode.com/2021/day/1) ⛴️🔈
 
-Another year of Advent of Nim :christmas_tree::crown:!
+Another year of [Advent of Nim](https://forum.nim-lang.org/t/8657) :christmas_tree::crown:!
 
 _This year I would like to try and use cool nim libraries
 for visualization_ and similar stuff.
@@ -105,8 +105,8 @@ doAssert input.window.countIncrease == 1608
 nbText: """### Optimizing part 2
 
 As hinted by [narimiran](https://github.com/narimiran) in our nim-aoc discord chat,
-there is a simpler way to compute part 2 that also reveals that the "window" approach
-does not really denoise the signal. Since two successive windows of 3 depths have 2 overlapping
+there is a simpler way to compute part 2 that also reveals that _the "window" approach
+does not really denoise the signal_. Since two successive windows of 3 depths have 2 overlapping
 depth, to check if there is an increase we only need to check first and last depth:
 """
 
@@ -118,47 +118,101 @@ nbCode:
 
   echo input.countIncrease2
 
-#doAssert input.countIncrease2 == 1608
+doAssert input.countIncrease2 == 1608
+# note that countIncrease2 is equivalent to countIncrease(..., window=1)
 
 nbText: """### Visualization
 
 I will use the excellent [ggplotnim](https://github.com/Vindaar/ggplotnim)
-to plot the depth profile.
+to plot the depth profile and the dataframe library [datamancer](https://github.com/SciNim/Datamancer)
+(implictly imported through ggplotnim) to manage the data.
+Thanks to [Vindaar](https://github.com/Vindaar) for help in this section
 """
 
 nbCode:
   import ggplotnim 
-  var df = toDf(input)
+  var df = seqsToDf({"depth": input})  # toDf(input): new column will be named input
   df["x"] = collect:
     for i in 1 .. input.len:
       i
   echo df
-  ggplot(df, aes(x="x", y="input")) + geom_line() + ggsave("2021/01_depths.png")
+  let
+    dark = parseHex("202b38")
+    gold = parseHex("ffff66")
+  ggplot(df, aes(x="x", y="depth")) + scale_y_reverse() +
+    geom_line(color=some(gold)) + theme_void(color=dark) +
+    ggsave("2021/01_depths.png")
+
 nbImage("2021/01_depths.png")
 
-nbText: "Interesting also to plot the relative depth (and its average, which from previous chart one can expect to be close to 4):"
+nbText: """
+Note:
+  - we use `seqsToDf` and a table constructor in order to give a new name to input
+  - we built (using sugar's collect) a dummy x axis in order to use it in the plots
+  - we reverse y scale to give the idea of increasing depth of sea bottom
+  - we use a gold line and same background as that of the page, removing all axis
+  - colors come from [chroma](https://github.com/treeform/chroma)
+
+Interesting also to plot the depth differences
+(and its average, which we can expect to be close to 4):
+"""
+
 nbCode:
-  df["relative_depth"] = collect:
+  df["diff_depth"] = collect:
     for i in 0 .. input.high:
       if i == 0:
         0
       else:
         input[i] - input[i-1]
-  let mean_relative_depth = mean(df["relative_depth"].toTensor(float))
-  dump mean_relative_depth
+  let mean_diff_depth = mean(df["diff_depth"].toTensor(float))
+  dump mean_diff_depth
   echo df
-  ggplot(df, aes(x="x", y="relative_depth")) +
-    geom_line() +
-    geom_linerange(aes = aes(y = mean_relative_depth, xMin = 0, xMax = 2000), color=some(parseHtmlName("red"))) +
+  let
+    green = parseHex("009900")
+    red = parseHtmlName("red")
+  let
+    font_aoc = Font(
+      family: "sans-serif",
+      size: 12.0,
+      bold: false,
+      slant: fsNormal,
+      color: green,  # only change with respect to default
+      alignKind: taCenter
+    )
+    theme_aoc = Theme(
+      canvasColor: some(dark),
+      plotBackgroundColor: some(dark),
+      gridLineColor: some(green),
+      labelFont: some(font_aoc),
+      tickLabelFont: some(font_aoc),
+      hideTicks: some(true)  # I cannot find where to set them as green, so I hide them
+    )
+  ggplot(df, aes(x="x", y="diff_depth")) +
+    geom_line(color=some(gold)) + theme_aoc +
+    geom_linerange(aes = aes(y = mean_diff_depth, xMin = 0, xMax = 2000), color=some(red)) +
     ggsave("2021/01_relative_depth.png")
+
 nbImage("2021/01_relative_depth.png")
 
 nbText: """
-Note that:
+Note:
   * to compute the mean we use `toTensor` and we convert to float (otherwise the result will be an `int`)
+  * we create a custom "aoc" theme setting appropriately colors
   * we are using `geom_linerange` to plot the horizontal line since `geom_hline` is not (yet) implemented
-  * we have a color as an `Option[chroma.Color]` (from [chroma](https://github.com/treeform/chroma) package)
+
+### highlights from subreddit
+
+Among the different stuff that is published in advent of code
+[subreddit](https://www.reddit.com/r/adventofcode/)
+I found interesting:
+
+  - a site that allows to [search all solutions](https://aocweb.yulrizka.com/?year=2021&day=1&language=Nim)
+  posted in the solution megathread by programming language ([announcement](https://www.reddit.com/r/adventofcode/comments/r6cmn1/aocweb_a_website_that_collects_solutions_from_the/))
+  - a [visualization](https://www.reddit.com/r/adventofcode/comments/r6asn2/2021_day_1_part_1well_its_a_game_turing_complete/) of a solution built with
+  [Turing Complete](https://store.steampowered.com/app/1444480/Turing_Complete/),
+  a game coded in Nim
 """
+
 #[
   add section of highlights from subreddit
 
