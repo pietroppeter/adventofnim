16f74286fc5d9588a93658e219850a42bdf37871
Pietro Peterlongo <pietro.peterlongo@gmail.com>
2021-12-09 18:42:09 +0100

    completed day09

--- 2021/day09.nim
+++ 2021/day09.nim
@@ -12,11 +12,14 @@ type
   Diff = array[Dir, int]
   GridDiff = Grid[Diff]
 
+func isOutside[T](c: Coord, g: Grid[T]): bool =
+  c.x < 0 or c.x >= g.xLen or c.y < 0 or c.y >= g.yLen
+
 func toIndex[T](g: Grid[T], c: Coord): int =
-  if c.x < 0 or c.x >= g.xLen or c.y < 0 or c.y >= g.yLen:
+  if c.isOutside(g):
     -1
   else:
-    c.y*g.yLen + c.x
+    c.y*g.xLen + c.x
 
 func `[]`[T](g: Grid[T], c: Coord): T =
   let i = g.toIndex(c)
@@ -33,7 +36,7 @@ func isLowPoint(gd: GridDiff, c: Coord): bool =
 
 func findAllLowPoints(gd: GridDiff): seq[Coord] =
   for y in 0 ..< gd.yLen:
-    for x in 0 ..< gd.yLen:
+    for x in 0 ..< gd.xLen:
       if gd.isLowPoint (x, y):
         result.add (x, y)
 
@@ -55,7 +58,7 @@ func parse(text: string): GridInt =
 func part1(g: GridInt, gd: GridDiff): int =
   let lowPoints = gd.findAlllowPoints
   for lowPoint in lowPoints:
-    result += g[lowPoint]
+    result += g[lowPoint] + 1
 
 iterator coords[T](g: Grid[T]): Coord =
   for y in 0 ..< g.yLen:
@@ -76,7 +79,9 @@ func `+`(c: Coord, dir: Dir): Coord =
 func computeDiff(g: GridInt): GridDiff =
   result.xLen = g.xLen
   result.yLen = g.ylen
-  result.data = newSeqWith(len=g.data.len): [0, 0, 0, 0]
+  result.data = @[[0, 0, 0, 0]]
+  for i in 1 ..< g.data.len:
+    result.data.add [0, 0, 0, 0]
   # sentinel not set (do not need it for the moment)
   for c in g.coords:
     for dir in Dir:
@@ -91,9 +96,41 @@ let
 8767896789
 9899965678"""
   testGrid = parse testInput
+  testGridDiff = testGrid.computeDiff
   puzzleInput = readFile("2021/input09.txt")
   puzzleGrid = parse puzzleInput
-  testGridDiff = testGrid.computeDiff
   puzzleGridDiff = puzzleGrid.computeDiff
 
 dump part1(testGrid, testGridDiff)
+dump part1(puzzleGrid, puzzleGridDiff)
+
+# part 2
+import sets, deques
+
+func findBasin(g: Grid, c: Coord): HashSet[Coord] =
+  result.incl c
+  var
+    queue = [c].toDeque
+    d, e: Coord
+  while queue.len > 0:
+    d = queue.popFirst
+    for dir in Dir:
+      e = d + dir
+      if not e.isOutside(g) and e not_in result and g[e] < 9:
+        result.incl e
+        queue.addLast e
+      
+func findAllBasins(g: Grid, gd: GridDiff): seq[HashSet[Coord]] =
+  let lowPoints = gd.findAllLowPoints
+  for lowPoint in lowPoints:
+    result.add g.findBasin lowPoint
+
+func getTop(s: seq[int], n: int): seq[int] =
+  assert s.len >= n
+  s.sorted[^n .. ^1]
+
+func part2(g: Grid, gd: GridDiff): int =
+  findAllBasins(g, gd).mapIt(it.len).getTop(3).prod
+
+dump part2(testGrid, testGridDiff)
+dump part2(puzzleGrid, puzzleGridDiff)
